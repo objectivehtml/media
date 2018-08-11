1f9212dc0de023dbc3322552fc0bcb04199991d0
Justin Kimbrell <jkimbrell@actengage.com>
2018-08-11 12:38:53 -0600

    Test config defaults

--- src/TemporaryModel.php
+++ src/TemporaryModel.php
@@ -4,13 +4,13 @@ namespace Objectivehtml\Media;
 
 class TemporaryModel extends Model
 {
-    
+
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
 
-        $this->disk = app(MediaService::class)->config('temp.disk');
-        $this->context = app(MediaService::class)->config('temp.context');
+        $this->disk = app(MediaService::class)->config('temp.disk', 'public');
+        $this->context = app(MediaService::class)->config('temp.context', '__temp__');
     }
 
 }
